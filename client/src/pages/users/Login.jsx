134233f6bf8429607ae6ae299deb5f056b0877b7
mmtonsing <langmuanmungtonxing@gmail.com>
2025-06-09 18:09:00 +0530

    fixing

--- client/src/pages/users/Login.jsx
+++ client/src/pages/users/Login.jsx
@@ -13,7 +13,7 @@ export default function Login() {
     e.preventDefault()
     try {
       const res = await axios.post(
-        '/auth/login',
+        `${import.meta.env.VITE_API_URL}/auth/login`,
         { username, password },
         { withCredentials: true }  //session
       );      
